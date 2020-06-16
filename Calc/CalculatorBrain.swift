26cc7866e9ac45b68b3a4266a145fb2bd7510ad4
marunomi <12097559+marunomi@users.noreply.github.com>
2020-06-16 21:23:24 +0900

    Initial Commit

---
+++ Calc/CalculatorBrain.swift
@@ -0,0 +1,90 @@
+//
+//  CalculatorBrain.swift
+//  Calc
+//
+//  Created by 新井まりな on 2020/06/16.
+//  Copyright © 2020 Marina Arai. All rights reserved.
+//
+
+import Foundation
+
+func changeSign(operand: Double) -> Double {
+    -operand
+}
+
+struct CaluclatorBrain {
+    private var accumlator: Double? //results of the calculations
+    
+    private enum Operation{
+        case constant(Double)
+        case unaryOperation( (Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "±": Operation.unaryOperation(changeSign),
+        "+": Operation.binaryOperation({$0 + $1}),
+        "-": Operation.binaryOperation({$0 - $1}),
+        "÷": Operation.binaryOperation({$0 / $1}),
+        "×": Operation.binaryOperation({$0 * $1}),
+        "=": Operation.equals,
+        
+    ]
+    
+    mutating func performOperation(_ symbol: String){
+        if let operation = operations[symbol]{
+            switch operation {
+            case .constant(let value):
+                accumlator = value
+            case .unaryOperation(let function):
+                if let accumlator = accumlator {
+                    self.accumlator = function(accumlator)
+                }
+                break
+            case .binaryOperation(let function):
+                if accumlator != nil{
+                    pbo = PendingBinaryOperation(function: function, firstOperand: accumlator!)
+                    accumlator = nil
+                }
+                break
+            case .equals:
+                performingPendingBinaryOperation()
+            }
+        }
+        
+    }
+    
+    private mutating func performingPendingBinaryOperation(){
+        if pbo != nil && accumlator != nil{
+            accumlator = pbo?.perform(with: accumlator!)
+            pbo = nil
+        }
+    }
+    
+    private var pbo: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation{
+        let function:(Double,Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double{
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double){
+        accumlator = operand
+    }
+    
+    var result: Double?{
+        get {
+            return accumlator
+        }
+    }
+    
+}
--- Calc/ContentView.swift
+++ Calc/ContentView.swift
@@ -8,14 +8,132 @@
 
 import SwiftUI
 
+class CalculatorViewModel: ObservableObject{
+    @Published var display = "0"
+    @Published var userIsInTheMiddleOfTyping = false
+    
+    var displayValue: Double {
+        get {
+            return Double(display)!
+        }
+        set {
+            display = String(newValue)
+        }
+    }
+}
+
 struct ContentView: View {
+    @EnvironmentObject var calculatorVM: CalculatorViewModel
+    @State var brain = CaluclatorBrain()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 8) {
+            Text(calculatorVM.display)
+                .font(.largeTitle)
+                .padding(.all)
+                .frame(maxWidth: .infinity,alignment: .trailing)
+                .border(Color.blue)
+                .padding(.leading)
+                .padding(.trailing)
+            
+            VStack(spacing: 8) {
+                BinarySmbols(brain: $brain)
+                HStack(spacing: 8){
+                    UnarySmbols(brain: $brain)
+                    Digits()
+                }
+            }
+            
+            
+        }
+    }
+}
+
+struct BinarySmbols: View {
+    @EnvironmentObject var calculatorVM: CalculatorViewModel
+    @Binding var brain:CaluclatorBrain
+    let symbols = ["+","-","÷","×"]
+    
+    var body: some View{
+        HStack(spacing:8) {
+            ForEach(symbols, id:\.self){ mathematicalSymbol in
+                Button(mathematicalSymbol, action: {
+                    self.calculatorVM.userIsInTheMiddleOfTyping = false
+                    self.brain.setOperand(self.calculatorVM.displayValue)
+                    self.brain.performOperation(mathematicalSymbol)
+                    if let result = self.brain.result {
+                        self.calculatorVM.displayValue = result
+                    }
+                })
+                    .frame(width: 64, height: 64)
+                    .border(Color.blue)
+            }
+        }
+    }
+}
+
+struct UnarySmbols: View {
+    @EnvironmentObject var calculatorVM: CalculatorViewModel
+    @Binding var brain:CaluclatorBrain
+    let symbols = ["π","√","±","="]
+    
+    var body: some View{
+        VStack(spacing:8) {
+            ForEach(symbols, id:\.self){ mathematicalSymbol in
+                Button(mathematicalSymbol, action: {
+                    self.calculatorVM.userIsInTheMiddleOfTyping = false
+                    self.brain.setOperand(self.calculatorVM.displayValue)
+                    self.brain.performOperation(mathematicalSymbol)
+                    if let result = self.brain.result {
+                        self.calculatorVM.displayValue = result
+                    }
+                })
+                    .frame(width: 64, height: 64)
+                    .border(Color.blue)
+            }
+        }
+    }
+}
+
+struct Digits: View {
+    @EnvironmentObject var calculatorVM: CalculatorViewModel
+    let digits = [[7,8,9],[4,5,6],[1,2,3],[0]]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            ForEach(digits, id:\.self){ rowDigits in
+                HStack(spacing: 8) {
+                    ForEach(rowDigits,id: \.self){ digit in
+                        Button("\(digit)",action: {
+                            if self.calculatorVM.userIsInTheMiddleOfTyping {
+                                self.calculatorVM.display = self.calculatorVM.display + "\(digit)"
+                            }else{
+                                self.calculatorVM.display = "\(digit)"
+                                self.calculatorVM.userIsInTheMiddleOfTyping = true
+                            }
+                        })
+                            .frame(width: 64, height: 64)
+                            .border(Color.blue)
+                    }
+                }
+            }
+            Button(".", action: {
+                if self.calculatorVM.userIsInTheMiddleOfTyping {
+                    self.calculatorVM.display = self.calculatorVM.display + "."
+                }else{
+                    self.calculatorVM.display = "0."
+                    self.calculatorVM.userIsInTheMiddleOfTyping = true
+                }
+            })
+            .frame(width: 64, height: 64)
+            .border(Color.blue)
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(CalculatorViewModel())
     }
 }
