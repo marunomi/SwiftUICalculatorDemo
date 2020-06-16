1c6626cf5585516eec3d99abb938c2a6aa2f8b90
marunomi <12097559+marunomi@users.noreply.github.com>
2020-06-16 15:53:10 +0900

    Initial Commit

---
+++ Calc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calc
+//
+//  Created by 新井まりな on 2020/06/16.
+//  Copyright © 2020 Marina Arai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
